602c84bf6d04ef0991bf8779802c065c956ac90e
soyoung <127925743+SeosoyoungE@users.noreply.github.com>
2023-12-18 06:14:08 +0000

    props를 사용하지않은 동적출력

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -1,11 +1,14 @@
 import "./ExpenseItem.css";
 function ExpenseItem() {
+  const expenseDate = new Date(2023, 11, 25);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
   return (
     <div className="expense-item">
-      <div>March 28th 2023</div>
-      <div>
-        <h2 className="expense-item__description">Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+      <div>{expenseDate.getDate()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
