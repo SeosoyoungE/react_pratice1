c6a8114e75f8f1a66b02dfa6329a83f58202af81
soyoung <127925743+SeosoyoungE@users.noreply.github.com>
2023-12-18 07:56:54 +0000

    년도수정

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -5,9 +5,16 @@ type Props = {
   amount: number;
 };
 function ExpenseItem(props: Props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
