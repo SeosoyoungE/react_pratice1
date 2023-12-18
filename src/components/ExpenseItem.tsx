f0e32d62df712db281ab0265cec0ccac288b6cc0
soyoung <127925743+SeosoyoungE@users.noreply.github.com>
2023-12-18 05:19:55 +0000

    add css

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -1,5 +1,14 @@
+import "./ExpenseItem.css";
 function ExpenseItem() {
-  return <h2>ExpenseItem!</h2>;
+  return (
+    <div className="expense-item">
+      <div>March 28th 2023</div>
+      <div>
+        <h2 className="expense-item__description">Car Insurance</h2>
+        <div className="expense-item__price">$294.67</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
