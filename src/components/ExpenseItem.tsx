e09aa7198348937d73d833ac390cbf042939e757
soyoung <127925743+SeosoyoungE@users.noreply.github.com>
2023-12-19 07:52:28 +0000

    expenseDate분리

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -1,20 +1,14 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 type Props = {
   date: Date;
   title: string;
   amount: number;
 };
 function ExpenseItem(props: Props) {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
